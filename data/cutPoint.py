712fd9e06cb765a4334ac77ea164806ffef6b386
Andrey Kuchebo <53372722+number3141@users.noreply.github.com>
2022-06-25 01:32:23 +0700

    2.10

--- data/cutPoint.py
+++ data/cutPoint.py
@@ -5,17 +5,22 @@ def cutPoint(match):
   """Получает один матч и возвращает очки хозяев и гостей
     Возвращаемое значение - ([очки хозяев], [очки гостей])
   """  
-  reg = re.compile('\d{2}')
   partHome = []
   partAway = []
-  # второй индекс не включается 
-  for index in range(1, 5): 
-    partPeriodHome = match.select('.event__part--home' + f'.event__part--{index}')
-    # Конверт в строку, потому что тип - ResultSet бьютифушный 
-    newNum = reg.findall(str(partPeriodHome))
-    partHome.append(int(*newNum))
-
-    partPeriodAway = match.select('.event__part--away' + f'.event__part--{index}')
-    newNum = reg.findall(str(partPeriodAway))
-    partAway.append(int(*newNum))
+  for part in range(1, 5): 
+    partPeriodHome = selectPointInPartByPlayField(match, 'home', part)
+    partHome.append(findDecimalNumImStr(partPeriodHome))
+    partPeriodAway = selectPointInPartByPlayField(match, 'away', part)
+    partAway.append(findDecimalNumImStr(partPeriodAway))
   return partHome, partAway 
+
+
+def selectPointInPartByPlayField(match, playField, part):
+  return match.select(f'.event__part--{playField}' + f'.event__part--{part}')
+
+def findDecimalNumImStr(findStr):
+   # Регулярная строка для поиска всех десятичных чисел 
+  reg = re.compile('\d{2}')
+  # Конверт в строку, потому что тип - ResultSet бьютифушный 
+  findPointInPart = reg.findall(str(findStr))
+  return int(*findPointInPart)
